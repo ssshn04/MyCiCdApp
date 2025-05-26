5bf470c873dcd46c3b178a021c3b2416d00e7992
ssshn04 <ssshn04@gmail.com>
2025-05-26 12:25:47 +0300

    first commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyCiCdApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IActionResult Get()
+        {
+            return Ok("Hello World from MyCiCdApp 🚀");
+        }
+
+    }
+}
